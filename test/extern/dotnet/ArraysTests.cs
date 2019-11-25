8706baec109c8d7729026d4e09320a7cd581a2eb
Robin Salkeld <salkeldr@amazon.com>
2019-11-25 14:25:03 -0800

    Add second project for tests

---
+++ test/extern/dotnet/ArraysTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+using NUnit.Framework;
+
+namespace EncryptionSDKTests
+{
+    [TestFixture]
+    public class ArraysTests
+    {
+        [Test]
+        public void CopyShouldWork()
+        {
+            var myArray = new int[] {1, 2, 3, 4, 5};
+            var copy = Arrays.Array.copy(myArray, new BigInteger(5));
+            Assert.AreEqual(copy, myArray);
+        }
+    }
+}
