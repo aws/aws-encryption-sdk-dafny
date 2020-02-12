6be47707855f57e04996fdbad0e418f953c52da9
Luke Maurer <maurerl@amazon.com>
2020-02-12 15:27:47 -0800

    Adapt to change in Dafny unboxed-java branch
    An array of generic type is now represented as a java.lang.Object
    rather than a dafny.Array.

--- src/extern/java/Arrays/Array.java
+++ src/extern/java/Arrays/Array.java
@@ -7,16 +7,18 @@ import java.math.BigInteger;
 public class Array {
   private Array() { }
 
-  public static <T> dafny.Array<T> copy(Type<T> t, dafny.Array<T> source, BigInteger length) {
-    if (length.intValue() == source.length) {
-      return source.clone();
+  public static <T> Object copy(Type<T> t, Object source, BigInteger length) {
+    assert t.arrayType().isInstance(source);
+
+    if (length.intValue() == t.getArrayLength(source)) {
+      return t.cloneArray(source);
     } else {
-      dafny.Array<T> dest = t.newArray(length.intValue());
-      source.copy(0, dest, 0, length.intValue());
+      Object dest = t.newArray(length.intValue());
+      t.copyArrayTo(source, 0, dest, 0, length.intValue());
       return dest;
     }
   }
-  public static <T> void copyTo(Type<T> t, dafny.Array<T> source, dafny.Array<T> dest, BigInteger offset) {
-    source.copy(0, dest, offset.intValue(), source.length);
+  public static <T> void copyTo(Type<T> t, Object source, Object dest, BigInteger offset) {
+    t.copyArrayTo(source, 0, dest, offset.intValue(), t.getArrayLength(source));
   }
 }
