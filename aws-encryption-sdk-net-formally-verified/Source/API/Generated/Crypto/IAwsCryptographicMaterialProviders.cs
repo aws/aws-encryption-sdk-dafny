131c435fd969820be702e42477fed80af0f56da0
texastony <5892063+texastony@users.noreply.github.com>
2021-12-20 11:15:24 -0600

    fix(Keyring): RawRSAKeyring example

--- aws-encryption-sdk-net-formally-verified/Source/API/Generated/Crypto/IAwsCryptographicMaterialProviders.cs
+++ aws-encryption-sdk-net-formally-verified/Source/API/Generated/Crypto/IAwsCryptographicMaterialProviders.cs
@@ -1,23 +1,14 @@
 // Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-
 using System;
-using Aws.Crypto;
-using
-    Aws.Crypto
-    ;
-
-namespace Aws.Crypto
-{
-    public interface IAwsCryptographicMaterialProviders
-    {
-        Aws.Crypto.IKeyring CreateMrkAwareStrictAwsKmsKeyring(Aws.Crypto.CreateMrkAwareStrictAwsKmsKeyringInput input);
-
-        Aws.Crypto.IKeyring CreateMultiKeyring(Aws.Crypto.CreateMultiKeyringInput input);
-
-        Aws.Crypto.IKeyring CreateRawAesKeyring(Aws.Crypto.CreateRawAesKeyringInput input);
-
-        Aws.Crypto.ICryptographicMaterialsManager CreateDefaultCryptographicMaterialsManager(
-            Aws.Crypto.CreateDefaultCryptographicMaterialsManagerInput input);
-    }
+ using Aws.Crypto;
+ using
+ Aws.Crypto
+ ; namespace Aws.Crypto {
+ public interface IAwsCryptographicMaterialProviders {
+ Aws.Crypto.IKeyring CreateMultiKeyring ( Aws.Crypto.CreateMultiKeyringInput input ) ;
+ Aws.Crypto.IKeyring CreateRawAesKeyring ( Aws.Crypto.CreateRawAesKeyringInput input ) ;
+ Aws.Crypto.IKeyring CreateRawRsaKeyring ( Aws.Crypto.CreateRawRsaKeyringInput input ) ;
+ Aws.Crypto.ICryptographicMaterialsManager CreateDefaultCryptographicMaterialsManager ( Aws.Crypto.CreateDefaultCryptographicMaterialsManagerInput input ) ;
+}
 }
