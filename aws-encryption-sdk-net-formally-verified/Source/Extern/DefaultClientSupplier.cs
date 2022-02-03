4c6ef618f5297ebb9867333ceb49b067273c15dd
texastony <5892063+texastony@users.noreply.github.com>
2022-02-03 17:02:47 -0600

    fix(DefaultClientSupplier):

--- aws-encryption-sdk-net-formally-verified/Source/Extern/DefaultClientSupplier.cs
+++ aws-encryption-sdk-net-formally-verified/Source/Extern/DefaultClientSupplier.cs
@@ -3,28 +3,24 @@
 
 using Amazon.KeyManagementService;
 using Amazon;
+using Aws.Crypto;
 
 using Wrappers_Compile;
-
 
 namespace DefaultClientSupplier {
 
     public partial class DefaultClientSupplier {
         public _IResult<Dafny.Com.Amazonaws.Kms.IKeyManagementServiceClient, Dafny.Aws.Crypto.IAwsCryptographicMaterialProvidersException> GetClient(Dafny.Aws.Crypto._IGetClientInput input)
         {
+            GetClientInput convertedInput = TypeConversion.FromDafny_N3_aws__N6_crypto__S14_GetClientInput(input);
             IAmazonKeyManagementService client;
-            if (input.Region != "")
-            {
-                var regionEndpoint = RegionEndpoint.GetBySystemName(input.Region);
+            if (convertedInput.Region != "") {
+                var regionEndpoint = RegionEndpoint.GetBySystemName(convertedInput.Region);
                 client = new AmazonKeyManagementServiceClient(regionEndpoint);
-            }
-            else
-            {
+            } else {
                 client = new AmazonKeyManagementServiceClient();
             }
-
-            return ToDafny_N3_aws__N6_crypto__S18_KmsClientReference(client);
-
+            return TypeConversion.ToDafny_N3_aws__N6_crypto__S18_KmsClientReference(client);
         }
     }
 }
