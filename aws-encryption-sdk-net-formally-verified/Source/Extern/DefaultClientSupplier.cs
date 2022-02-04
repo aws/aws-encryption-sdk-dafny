d6afe8dfd2195d986e786c65c8754b0095a99233
texastony <5892063+texastony@users.noreply.github.com>
2022-02-04 17:47:18 -0600

    fix(DefaultClientSupplier): cast error

--- aws-encryption-sdk-net-formally-verified/Source/Extern/DefaultClientSupplier.cs
+++ aws-encryption-sdk-net-formally-verified/Source/Extern/DefaultClientSupplier.cs
@@ -3,6 +3,7 @@
 
 using Amazon.KeyManagementService;
 using Amazon;
+using Amazon.Runtime;
 using Aws.Crypto;
 
 using Wrappers_Compile;
@@ -12,17 +13,33 @@ namespace DefaultClientSupplier {
     public partial class DefaultClientSupplier {
         public _IResult<Dafny.Com.Amazonaws.Kms.IKeyManagementServiceClient, Dafny.Aws.Crypto.IAwsCryptographicMaterialProvidersException> GetClient(Dafny.Aws.Crypto._IGetClientInput input)
         {
-            GetClientInput convertedInput = TypeConversion.FromDafny_N3_aws__N6_crypto__S14_GetClientInput(input);
-            IAmazonKeyManagementService client;
-            if (convertedInput.Region != "") {
-                var regionEndpoint = RegionEndpoint.GetBySystemName(convertedInput.Region);
-                client = new AmazonKeyManagementServiceClient(regionEndpoint);
-            } else {
-                client = new AmazonKeyManagementServiceClient();
+            try
+            {
+                GetClientInput convertedInput = TypeConversion.FromDafny_N3_aws__N6_crypto__S14_GetClientInput(input);
+                IAmazonKeyManagementService client;
+                if (convertedInput.Region != "")
+                {
+                    var regionEndpoint = RegionEndpoint.GetBySystemName(convertedInput.Region);
+                    client = new AmazonKeyManagementServiceClient(regionEndpoint);
+                }
+                else
+                {
+                    client = new AmazonKeyManagementServiceClient();
+                }
+
+                return Result<Dafny.Com.Amazonaws.Kms.IKeyManagementServiceClient,
+                    Dafny.Aws.Crypto.IAwsCryptographicMaterialProvidersException>.create_Success(
+                    TypeConversion.ToDafny_N3_aws__N6_crypto__S15_GetClientOutput(client)
+                );
             }
-            return Result<Dafny.Com.Amazonaws.Kms.IKeyManagementServiceClient, Dafny.Aws.Crypto.IAwsCryptographicMaterialProvidersException>.create_Success(
-                TypeConversion.ToDafny_N3_aws__N6_crypto__S15_GetClientOutput(client)
-            );
+            catch (AmazonServiceException e)
+            {
+                return Result<Dafny.Com.Amazonaws.Kms.IKeyManagementServiceClient,
+                    Dafny.Aws.Crypto.IAwsCryptographicMaterialProvidersException>.create_Failure(
+                    new AwsCryptographicMaterialProvidersClientException(e.Message)
+                );
+            }
+
         }
     }
 }
