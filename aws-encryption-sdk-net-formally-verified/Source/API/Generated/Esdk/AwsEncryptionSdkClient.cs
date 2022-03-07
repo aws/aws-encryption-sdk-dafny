c232a77ab6b09834c55ccff7025a07d79a1858c6
Benjamin Farley <farleyb@amazon.com>
2022-03-07 15:16:35 -0700

    space

--- aws-encryption-sdk-net-formally-verified/Source/API/Generated/Esdk/AwsEncryptionSdkClient.cs
+++ aws-encryption-sdk-net-formally-verified/Source/API/Generated/Esdk/AwsEncryptionSdkClient.cs
@@ -17,6 +17,7 @@ namespace Aws.Esdk
 
         public AwsEncryptionSdkClient(Aws.Esdk.AwsEncryptionSdkClientConfig config) : base(config)
         {
+            // TODO: these lines were manually updated
             this._impl =
                 new Dafny.Aws.Esdk.AwsEncryptionSdkClient.AwsEncryptionSdkClient();
             this._impl.__ctor(TypeConversion.ToDafny_N3_aws__N4_esdk__S28_AwsEncryptionSdkClientConfig(config));
