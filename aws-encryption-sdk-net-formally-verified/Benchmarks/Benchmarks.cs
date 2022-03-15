7839533e8482e12db13332e7e7b4dd85789de34e
Alex Chew <alexchew@amazon.com>
2022-03-14 17:47:05 -0700

    add raw RSA keyring benchmark

--- aws-encryption-sdk-net-formally-verified/Benchmarks/Benchmarks.cs
+++ aws-encryption-sdk-net-formally-verified/Benchmarks/Benchmarks.cs
@@ -6,15 +6,14 @@ using Aws.Esdk;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 using Org.BouncyCastle.Utilities.Encoders;
+using RSA = RSAEncryption.RSA;
 
 namespace Benchmarks
 {
-    public class EncryptDecryptBenchmark
+    public abstract class BaseEncryptDecryptBenchmark
     {
         [Params(0, 10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000)]
         public int PlaintextLengthBytes { get; set; }
-
-        private static readonly string AES_256_KEY_MATERIAL_B64 = "AAECAwQFBgcICRAREhMUFRYXGBkgISIjJCUmJygpMDE=";
 
         private IAwsEncryptionSdk _encryptionSdk;
         private IKeyring _keyring;
@@ -22,20 +21,18 @@ namespace Benchmarks
         private MemoryStream _ciphertext;
         private Dictionary<string, string> _encryptionContext;
 
+        /**
+         * Concrete benchmark classes should implement this method.
+         */
+        protected abstract IKeyring CreateKeyring();
+
         // Runs once for each combination of params
         [GlobalSetup]
         public void GlobalSetup()
         {
             _encryptionSdk = AwsEncryptionSdkFactory.CreateDefaultAwsEncryptionSdk();
 
-            var providers = AwsCryptographicMaterialProvidersFactory.CreateDefaultAwsCryptographicMaterialProviders();
-            _keyring = providers.CreateRawAesKeyring(new CreateRawAesKeyringInput
-            {
-                KeyNamespace = "aes_namespace",
-                KeyName = "aes_name",
-                WrappingAlg = AesWrappingAlg.ALG_AES256_GCM_IV12_TAG16,
-                WrappingKey = new MemoryStream(Base64.Decode(AES_256_KEY_MATERIAL_B64))
-            });
+            _keyring = CreateKeyring();
 
             _plaintext = new MemoryStream(PlaintextLengthBytes);
             _plaintext.SetLength(PlaintextLengthBytes);  // need to set this because buffer is 0-length by default
@@ -68,6 +65,41 @@ namespace Benchmarks
                 Keyring = _keyring,
                 Ciphertext = _ciphertext
             });
+    }
+
+    public class RawAesKeyringBenchmark : BaseEncryptDecryptBenchmark
+    {
+        private static readonly string AES_256_KEY_MATERIAL_B64 = "AAECAwQFBgcICRAREhMUFRYXGBkgISIjJCUmJygpMDE=";
+
+        protected override IKeyring CreateKeyring()
+        {
+            var providers = AwsCryptographicMaterialProvidersFactory.CreateDefaultAwsCryptographicMaterialProviders();
+            return providers.CreateRawAesKeyring(new CreateRawAesKeyringInput
+            {
+                KeyNamespace = "aes_namespace",
+                KeyName = "aes_name",
+                WrappingAlg = AesWrappingAlg.ALG_AES256_GCM_IV12_TAG16,
+                WrappingKey = new MemoryStream(Base64.Decode(AES_256_KEY_MATERIAL_B64))
+            });
+        }
+    }
+
+    public class RawRsaKeyringBenchmark : BaseEncryptDecryptBenchmark
+    {
+        protected override IKeyring CreateKeyring()
+        {
+            RSA.GenerateKeyPairBytes(2048, out var publicKeyBytes, out var privateKeyBytes);
+
+            var providers = AwsCryptographicMaterialProvidersFactory.CreateDefaultAwsCryptographicMaterialProviders();
+            return providers.CreateRawRsaKeyring(new CreateRawRsaKeyringInput
+            {
+                KeyNamespace = "rsa_namespace",
+                KeyName = "rsa_name",
+                PaddingScheme = PaddingScheme.OAEP_SHA512_MGF1,
+                PublicKey = new MemoryStream(publicKeyBytes),
+                PrivateKey = new MemoryStream(privateKeyBytes)
+            });
+        }
     }
 
     public class Program
