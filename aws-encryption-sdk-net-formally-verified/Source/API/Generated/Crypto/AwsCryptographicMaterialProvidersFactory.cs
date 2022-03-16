be3ce48690808b832a474ab969e2472ae6ac0aa4
Benjamin Farley <farleyb@amazon.com>
2022-03-16 13:46:42 -0600

    fix

--- aws-encryption-sdk-net-formally-verified/Source/API/Generated/Crypto/AwsCryptographicMaterialProvidersFactory.cs
+++ aws-encryption-sdk-net-formally-verified/Source/API/Generated/Crypto/AwsCryptographicMaterialProvidersFactory.cs
@@ -13,12 +13,13 @@ namespace Aws.EncryptionSdk.Core
 {
     public static class AwsCryptographicMaterialProvidersFactory
     {
-        static Dafny.Aws.EncryptionSdk.Core.AwsCryptographicMaterialProvidersFactory.AwsCryptographicMaterialProvidersFactory
-            _impl =
+        static Dafny.Aws.EncryptionSdk.Core.AwsCryptographicMaterialProvidersFactory.
+            AwsCryptographicMaterialProvidersFactory _impl =
                 new Dafny.Aws.EncryptionSdk.Core.AwsCryptographicMaterialProvidersFactory.
                     AwsCryptographicMaterialProvidersFactory();
 
-        public static Aws.EncryptionSdk.Core.IAwsCryptographicMaterialProviders CreateDefaultAwsCryptographicMaterialProviders()
+        public static Aws.EncryptionSdk.Core.IAwsCryptographicMaterialProviders
+            CreateDefaultAwsCryptographicMaterialProviders()
         {
             Wrappers_Compile._IResult<Dafny.Aws.EncryptionSdk.Core.IAwsCryptographicMaterialProviders,
                 Dafny.Aws.EncryptionSdk.Core.IAwsCryptographicMaterialProvidersException> result =
@@ -26,8 +27,9 @@ namespace Aws.EncryptionSdk.Core
             if (result.is_Failure)
                 throw TypeConversion.FromDafny_CommonError_AwsCryptographicMaterialProvidersBaseException(
                     result.dtor_error);
-            return TypeConversion.FromDafny_N3_aws__N13_encryptionSdk__N4_core__S42_AwsCryptographicMaterialProvidersReference(
-                result.dtor_value);
+            return TypeConversion
+                .FromDafny_N3_aws__N13_encryptionSdk__N4_core__S42_AwsCryptographicMaterialProvidersReference(
+                    result.dtor_value);
         }
     }
 }
--- aws-encryption-sdk-net-formally-verified/Source/API/Generated/Crypto/Keyring.cs
+++ aws-encryption-sdk-net-formally-verified/Source/API/Generated/Crypto/Keyring.cs
@@ -4,7 +4,6 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
-using Aws.EncryptionSdk;
 using Aws.EncryptionSdk.Core;
 using
     Aws.EncryptionSdk.Core
