1a32dbef97fcbdbe26dccd0685e4595a35a10318
texastony <5892063+texastony@users.noreply.github.com>
2022-04-08 15:47:32 -0700

    feat(Examples): Default CMM example

---
+++ aws-encryption-sdk-net-formally-verified/Examples/CryptographicMaterialsManager/DefaultCryptographicMaterialsManagerExample.cs
@@ -0,0 +1,97 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using AWS.EncryptionSDK;
+using AWS.EncryptionSDK.Core;
+using Xunit;
+using static ExampleUtils.ExampleUtils;
+
+/// Demonstrate an encrypt/decrypt cycle using a Default Cryptographic Materials Manager.
+/// Cryptographic material managers (CMMs) give you higher-level controls than keyrings
+/// over how the AWS Encryption SDK protects your data.
+/// For the sake of convenience, the Encryption SDK will provision the Default CMM
+/// if a keyring is provided to the Encrypt or Decrypt APIs.
+/// As such, this example executes the same as the RawAESKeyringExample.
+public class DefaultCryptographicMaterialsManagerExample
+{
+    private static void Run(MemoryStream plaintext)
+    {
+        // Create your encryption context.
+        // Remember that your encryption context is NOT SECRET.
+        // https://docs.aws.amazon.com/encryption-sdk/latest/developer-guide/concepts.html#encryption-context
+        var encryptionContext = new Dictionary<string, string>()
+        {
+            {"encryption", "context"},
+            {"is not", "secret"},
+            {"but adds", "useful metadata"},
+            {"that can help you", "be confident that"},
+            {"the data you are handling", "is what you think it is"}
+        };
+
+        // Instantiate the Material Providers and the AWS Encryption SDK
+        var materialProviders =
+            AwsCryptographicMaterialProvidersFactory.CreateDefaultAwsCryptographicMaterialProviders();
+        var encryptionSdk = AwsEncryptionSdkFactory.CreateDefaultAwsEncryptionSdk();
+
+        // Create a DefaultCryptographicMaterialsManager
+        var cmmInput = new CreateDefaultCryptographicMaterialsManagerInput
+        {
+            // Create a keyring via a helper method.
+            Keyring = GetRawAESKeyring(materialProviders)
+        };
+        var cmm = materialProviders.CreateDefaultCryptographicMaterialsManager(cmmInput);
+
+        // Encrypt your plaintext data.
+        var encryptInput = new EncryptInput
+        {
+            Plaintext = plaintext,
+            MaterialsManager = cmm,
+            EncryptionContext = encryptionContext
+        };
+        var encryptOutput = encryptionSdk.Encrypt(encryptInput);
+        var ciphertext = encryptOutput.Ciphertext;
+
+        // Demonstrate that the ciphertext and plaintext are different.
+        Assert.NotEqual(ciphertext.ToArray(), plaintext.ToArray());
+
+        // Decrypt your encrypted data using the same cryptographic material manager
+        // you used on encrypt.
+        //
+        // You do not need to specify the encryption context on decrypt
+        // because the header of the encrypted message includes the encryption context.
+        var decryptInput = new DecryptInput
+        {
+            Ciphertext = ciphertext,
+            MaterialsManager = cmm
+        };
+        var decryptOutput = encryptionSdk.Decrypt(decryptInput);
+
+        // Before your application uses plaintext data, verify that the encryption context that
+        // you used to encrypt the message is included in the encryption context that was used to
+        // decrypt the message. The AWS Encryption SDK can add pairs, so don't require an exact match.
+        //
+        // In production, always use a meaningful encryption context.
+        foreach (var expectedPair in encryptionContext)
+        {
+            if (!decryptOutput.EncryptionContext.TryGetValue(expectedPair.Key, out var decryptedValue)
+                || !decryptedValue.Equals(expectedPair.Value))
+            {
+                throw new Exception("Encryption context does not match expected values");
+            }
+        }
+
+        // Demonstrate that the decrypted plaintext is identical to the original plaintext.
+        var decrypted = decryptOutput.Plaintext;
+        Assert.Equal(decrypted.ToArray(), plaintext.ToArray());
+    }
+
+    // We test examples to ensure they remain up-to-date.
+    [Fact]
+    public void TestDefaultCryptographicMaterialsManagerExample()
+    {
+        Run(GetPlaintextStream());
+    }
+}
