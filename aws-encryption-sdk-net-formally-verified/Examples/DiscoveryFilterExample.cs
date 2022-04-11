b34de3e4cbf632bbf52921547bd06a1c760ee049
texastony <5892063+texastony@users.noreply.github.com>
2022-04-11 14:27:47 -0700

    feat(Examples): Discovery Filter

---
+++ aws-encryption-sdk-net-formally-verified/Examples/DiscoveryFilterExample.cs
@@ -0,0 +1,184 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Amazon.KeyManagementService;
+using AWS.EncryptionSDK;
+using AWS.EncryptionSDK.Core;
+using Xunit;
+using static ExampleUtils.ExampleUtils;
+
+/// <para>
+/// Demonstrate using Discovery Filters.
+/// Discovery Filters are used to restrict Discovery Keyrings to trusted AWS
+/// Accounts.
+/// The Accounts are specified by their Account Ids and the partition they are
+/// in.
+/// Using Discovery Filters with Discovery Keyrings is a best practice.
+/// Particularly if an application is decrypting messages from untrusted sources,
+/// limiting the AWS Accounts a KMS Key can be sourced from prevents calls to
+/// KMS that may fail due to permission issues.
+/// Of course, a malicious actor could encrypt data under a KMS Key that allows any
+/// AWS Account to decrypt it. Then the malicious message would successfully
+/// be decrypted, which could be harmful.
+/// </para>
+public class DiscoveryFilterExample
+{
+    /// <param name="plaintext">Data to be encrypted</param>
+    /// <param name="encryptKeyArn">KMS Key Arn to encrypt the plaintext under</param>
+    /// <param name="trustedAccountIds">List of AWS Account Ids that are trusted.</param>
+    /// <param name="awsPartition">AWS Partition that contains all the members of "trustedAccountIds".</param>
+    /// <exception cref="Exception"></exception>
+    private static void Run(
+        MemoryStream plaintext,
+        string encryptKeyArn,
+        List<string> trustedAccountIds,
+        string awsPartition
+    )
+    {
+        // Instantiate the Material Providers and the AWS Encryption SDK
+        var materialProviders =
+            AwsCryptographicMaterialProvidersFactory.CreateDefaultAwsCryptographicMaterialProviders();
+        var encryptionSdk = AwsEncryptionSdkFactory.CreateDefaultAwsEncryptionSdk();
+
+        // To focus on Discovery Filters, we will rely on a helper method
+        // to create the encrypted message (ciphertext).
+        Dictionary<string, string> encryptionContext;
+        MemoryStream ciphertext;
+        (encryptionContext, ciphertext) = EncryptMessageWithKMSKey(
+            plaintext, encryptKeyArn, materialProviders, encryptionSdk);
+
+        // Now create a Discovery keyring to use for decryption.
+        // We'll add a discovery filter so that we limit the set of Encrypted Data Keys
+        // we are willing to decrypt to only ones created by KMS keys from
+        // trusted accounts.
+        var decryptKeyringInput = new CreateAwsKmsDiscoveryKeyringInput
+        {
+            KmsClient = new AmazonKeyManagementServiceClient(),
+            DiscoveryFilter = new DiscoveryFilter
+            {
+                AccountIds = trustedAccountIds,
+                Partition = awsPartition
+            }
+        };
+        var decryptKeyring = materialProviders.CreateAwsKmsDiscoveryKeyring(decryptKeyringInput);
+
+        var decryptInput = new DecryptInput
+        {
+            Ciphertext = ciphertext,
+            Keyring = decryptKeyring
+        };
+        // If the `encryptKeyArn` is from an AWS Account in `trustedAccountIds`,
+        // then this decryption will succeed.
+        var decryptOutput = encryptionSdk.Decrypt(decryptInput);
+        VerifyEncryptionContext(decryptOutput, encryptionContext);
+        VerifyDecryptedIsPlaintext(decryptOutput, plaintext);
+
+        // If we create a Discovery Filter that excludes the account the
+        // `encryptKeyArn` is from, the decryption will fail.
+        var decryptFailed = false;
+        decryptKeyringInput.DiscoveryFilter = new DiscoveryFilter
+        {
+            AccountIds = new List<string>() {"123456789012"},
+            Partition = awsPartition
+        };
+        var failingKeyring = materialProviders.CreateAwsKmsDiscoveryKeyring(decryptKeyringInput);
+        decryptInput.Keyring = failingKeyring;
+        try
+        {
+            encryptionSdk.Decrypt(decryptInput);
+        }
+        catch (AwsEncryptionSdkException)
+        {
+            decryptFailed = true;
+        }
+
+        Assert.True(decryptFailed);
+
+    }
+
+    /// <summary>
+    ///     To focus on Discovery Filters, we rely on this helper method
+    ///     to create the encrypted message (ciphertext) with the given KMS Key.
+    /// </summary>
+    private static (Dictionary<string, string> encryptionContext, MemoryStream ciphertext) EncryptMessageWithKMSKey(
+        MemoryStream plaintext, string kmsKeyArn, IAwsCryptographicMaterialProviders materialProviders,
+        IAwsEncryptionSdk encryptionSdk)
+    {
+        // Create your encryption context.
+        // Remember that your encryption context is NOT SECRET.
+        // https://docs.aws.amazon.com/encryption-sdk/latest/developer-guide/concepts.html#encryption-context
+        var encryptionContext = new Dictionary<string, string>
+        {
+            {"encryption", "context"},
+            {"is not", "secret"},
+            {"but adds", "useful metadata"},
+            {"that can help you", "be confident that"},
+            {"the data you are handling", "is what you think it is"}
+        };
+
+        // Create the keyring that determines how your data keys are protected.
+        // Though this example highlights Discovery Filters, Discovery keyrings
+        // cannot be used to encrypt, so for encryption we create
+        // a KMS keyring without discovery mode.
+        var createKeyringInput = new CreateAwsKmsKeyringInput
+        {
+            KmsClient = new AmazonKeyManagementServiceClient(),
+            KmsKeyId = kmsKeyArn
+        };
+        var encryptKeyring = materialProviders.CreateAwsKmsKeyring(createKeyringInput);
+
+        // Encrypt your plaintext data.
+        var encryptInput = new EncryptInput
+        {
+            Plaintext = plaintext,
+            Keyring = encryptKeyring,
+            EncryptionContext = encryptionContext
+        };
+        var encryptOutput = encryptionSdk.Encrypt(encryptInput);
+        var ciphertext = encryptOutput.Ciphertext;
+        return (encryptionContext, ciphertext);
+    }
+
+    /// <summary>
+    ///     For this example, we break out the Encryption Context Verification
+    ///     into a helper method.
+    ///     While Encryption Context Verification is a best practice, it is not
+    ///     the topic of this example.
+    /// </summary>
+    private static void VerifyEncryptionContext(
+        DecryptOutput decryptOutput,
+        Dictionary<string, string> encryptionContext
+    )
+    {
+        // Before your application uses plaintext data, verify that the encryption context that
+        // you used to encrypt the message is included in the encryption context that was used to
+        // decrypt the message. The AWS Encryption SDK can add pairs, so don't require an exact match.
+        //
+        // In production, always use a meaningful encryption context.
+        foreach (var expectedPair in encryptionContext)
+            if (!decryptOutput.EncryptionContext.TryGetValue(expectedPair.Key, out var decryptedValue)
+                || !decryptedValue.Equals(expectedPair.Value))
+                throw new Exception("Encryption context does not match expected values");
+    }
+
+    /// <summary>
+    ///     This is helper method that ensures the decrypted message is the
+    ///     same as the encrypted message.
+    /// </summary>
+    private static void VerifyDecryptedIsPlaintext(DecryptOutput decryptOutput, MemoryStream plaintext)
+    {
+        // Demonstrate that the decrypted plaintext is identical to the original plaintext.
+        var decrypted = decryptOutput.Plaintext;
+        Assert.Equal(decrypted.ToArray(), plaintext.ToArray());
+    }
+
+    // We test examples to ensure they remain up-to-date.
+    [Fact]
+    public void TestDiscoveryFilterExample()
+    {
+        Run(GetPlaintextStream(), GetDefaultRegionKmsKeyArn(), GetAccountIds(), "aws");
+    }
+}
