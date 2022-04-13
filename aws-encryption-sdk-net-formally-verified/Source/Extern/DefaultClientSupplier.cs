e149789204aaf0709510bc66d32f474ebc82eedc
texastony <5892063+texastony@users.noreply.github.com>
2022-04-13 13:18:26 -0700

    WIP: manual generation to support custom client supplier

--- aws-encryption-sdk-net-formally-verified/Source/Extern/DefaultClientSupplier.cs
+++ aws-encryption-sdk-net-formally-verified/Source/Extern/DefaultClientSupplier.cs
@@ -1,21 +1,27 @@
 // Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-using Amazon.KeyManagementService;
 using Amazon;
+using Amazon.KeyManagementService;
 using Amazon.Runtime;
+// ReSharper disable once RedundantUsingDirective
 using AWS.EncryptionSDK.Core;
 
+// ReSharper disable once RedundantUsingDirective
 using Wrappers_Compile;
 
-namespace DefaultClientSupplier {
-
-    public partial class DefaultClientSupplier {
-        public _IResult<Dafny.Com.Amazonaws.Kms.IKeyManagementServiceClient, Dafny.Aws.EncryptionSdk.Core.IAwsCryptographicMaterialProvidersException> GetClient(Dafny.Aws.EncryptionSdk.Core._IGetClientInput input)
+namespace DefaultClientSupplier
+{
+    public partial class DefaultClientSupplier : Dafny.Aws.EncryptionSdk.Core.IClientSupplier
+    {
+        public _IResult<Dafny.Com.Amazonaws.Kms.IKeyManagementServiceClient,
+            Dafny.Aws.EncryptionSdk.Core.IAwsCryptographicMaterialProvidersException> GetClient(
+            Dafny.Aws.EncryptionSdk.Core._IGetClientInput input)
         {
+            GetClientInput convertedInput =
+                TypeConversion.FromDafny_N3_aws__N13_encryptionSdk__N4_core__S14_GetClientInput(input);
             try
             {
-                GetClientInput convertedInput = TypeConversion.FromDafny_N3_aws__N13_encryptionSdk__N4_core__S14_GetClientInput(input);
                 IAmazonKeyManagementService client;
                 if (convertedInput.Region != "")
                 {
@@ -36,10 +42,10 @@ namespace DefaultClientSupplier {
             {
                 return Result<Dafny.Com.Amazonaws.Kms.IKeyManagementServiceClient,
                     Dafny.Aws.EncryptionSdk.Core.IAwsCryptographicMaterialProvidersException>.create_Failure(
-                    TypeConversion.ToDafny_CommonError_AwsCryptographicMaterialProvidersBaseException(new AwsCryptographicMaterialProvidersException(e.Message))
+                    TypeConversion.ToDafny_CommonError_AwsCryptographicMaterialProvidersBaseException(
+                        new AwsCryptographicMaterialProvidersException(e.Message))
                 );
             }
-
         }
     }
 }
