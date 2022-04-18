06a49c4f03ea070184d93f67f500345ef8e94f74
Benjamin Farley <farleyb@amazon.com>
2022-04-18 15:16:58 -0600

    refactors

--- aws-encryption-sdk-net-formally-verified/codebuild/release/upload_assembly.py
+++ aws-encryption-sdk-net-formally-verified/codebuild/release/upload_assembly.py
@@ -3,4 +3,4 @@ from signing_lib import upload_assembly, parse_args
 
 if __name__ == '__main__':
     args = parse_args()
-    upload_assembly(args.target, args.unique_id)
+    upload_assembly(args)
