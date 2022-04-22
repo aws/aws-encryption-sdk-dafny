31426e9d7b5a63f4344d9208455d75379bcea916
texastony <5892063+texastony@users.noreply.github.com>
2022-04-21 20:22:22 -0700

    test(NET): Type Conversion rejects Interface

---
+++ aws-encryption-sdk-net-formally-verified/Test/NativeWrappers/TypeConversionToDafnyWrapperTest.cs
@@ -0,0 +1,49 @@
+using System;
+using AWS.EncryptionSDK.Core;
+using Xunit;
+using Xunit.Sdk;
+
+namespace AWSEncryptionSDKTests.NativeWrappers
+{
+    public class TypeConversionToDafnyWrapperTest
+    {
+        [Fact]
+        public void TestInterfaceThrowsException()
+        {
+            const string expectedMessage =
+                "Custom implementations of Keyring should extend KeyringBase.";
+            var expected = new ArgumentException(expectedMessage);
+            Dafny.Aws.EncryptionSdk.Core.IKeyring output = null;
+            try
+            {
+                output = TypeConversion
+                    .ToDafny_N3_aws__N13_encryptionSdk__N4_core__S16_KeyringReference(
+                        new KeyringInterface());
+            }
+            catch (ArgumentException e)
+            {
+                Assert.Equal(expectedMessage, e.Message);
+                return;
+            }
+            catch (Exception e)
+            {
+                throw new XunitException(
+                    $"TypeConversion.ToDafny_N3_aws__N13_encryptionSdk__N4_core__S16_KeyringReference" +
+                    $" through an exception: {e.GetType()} with message: \"{e.Message}\"." +
+                    $"Should have thrown: \"{expected}\""
+                );
+            }
+            throw new XunitException(
+                "TypeConversion.ToDafny_N3_aws__N13_encryptionSdk__N4_core__S16_KeyringReference" +
+                $" returned {output}. Should have thrown: \"{expected}\"");
+        }
+    }
+
+    class KeyringInterface : IKeyring
+    {
+        public OnEncryptOutput OnEncrypt(OnEncryptInput input)
+        { throw new System.NotImplementedException(); }
+        public OnDecryptOutput OnDecrypt(OnDecryptInput input)
+        { throw new System.NotImplementedException(); }
+    }
+}
