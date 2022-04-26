fe3d024ad150657ec8c210e9893c846fa7662999
Tony Knapp <5892063+texastony@users.noreply.github.com>
2022-04-26 13:58:03 -0700

    docs(Examples): DiscoveryFilter usage example
    Co-authored-by: seebees <ryanemer@amazon.com>

--- aws-encryption-sdk-net-formally-verified/Examples/DiscoveryFilterExample.cs
+++ aws-encryption-sdk-net-formally-verified/Examples/DiscoveryFilterExample.cs
@@ -25,8 +25,8 @@ using static ExampleUtils.ExampleUtils;
 /// metadata of the encrypted data key.
 /// When specifying AWS KMS wrapping keys for decrypting is impractical (such as when
 /// encrypting using AWS KMS Aliases), you can use discovery keyrings.
-/// If using discovery keyrings (which is not a best practice, but is necessary in
-/// some occasions), using Discovery Filters is a best practice.
+/// When you can not specify your wrapping keys explicitly,
+/// using a Discovery Filter is a best practice.
 /// Particularly if an application is decrypting messages from multiple sources,
 /// adding trusted AWS accounts to the discovery filter allows it to
 /// protect itself from decrypting messages from untrusted sources.
