d81725d0c5f340df8f56b24063b8233d746c14fc
Alex Chew <alexchew@amazon.com>
2022-07-12 11:26:16 -0700

    add user agent to default KMS clients

--- aws-encryption-sdk-net/Source/Extern/DefaultClientSupplier.cs
+++ aws-encryption-sdk-net/Source/Extern/DefaultClientSupplier.cs
@@ -1,9 +1,14 @@
 // Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
 using Amazon;
 using Amazon.KeyManagementService;
 using Amazon.Runtime;
+using Amazon.Runtime.Internal;
+using Amazon.Util;
 // ReSharper disable once RedundantUsingDirective
 using AWS.EncryptionSDK.Core;
 
@@ -24,16 +29,14 @@ namespace DefaultClientSupplier
                 AWS.EncryptionSDK.Core.TypeConversion.FromDafny_N3_aws__N13_encryptionSdk__N4_core__S14_GetClientInput(input);
             try
             {
-                IAmazonKeyManagementService client;
-                if (convertedInput.Region != "")
+                var regionEndpoint = string.IsNullOrEmpty(convertedInput.Region)
+                    ? null
+                    : RegionEndpoint.GetBySystemName(convertedInput.Region);
+                var clientConfig = new AmazonKeyManagementServiceConfig
                 {
-                    var regionEndpoint = RegionEndpoint.GetBySystemName(convertedInput.Region);
-                    client = new AmazonKeyManagementServiceClient(regionEndpoint);
-                }
-                else
-                {
-                    client = new AmazonKeyManagementServiceClient();
-                }
+                    RegionEndpoint = regionEndpoint
+                };
+                var client = new DefaultKmsClient(clientConfig);
 
                 // ReSharper disable once RedundantNameQualifier
                 return Wrappers_Compile.Result<Dafny.Com.Amazonaws.Kms.IKeyManagementServiceClient,
@@ -48,6 +51,59 @@ namespace DefaultClientSupplier
                     AWS.EncryptionSDK.Core.TypeConversion.ToDafny_CommonError(e)
                 );
             }
+        }
+    }
+
+    /// <summary>
+    /// A KMS client that adds the Encryption SDK version to the user agent.
+    /// </summary>
+    internal class DefaultKmsClient : AmazonKeyManagementServiceClient
+    {
+        public DefaultKmsClient(AmazonKeyManagementServiceConfig config) : base(config)
+        {
+        }
+
+        protected override void CustomizeRuntimePipeline(RuntimePipeline pipeline)
+        {
+            base.CustomizeRuntimePipeline(pipeline);
+            pipeline.AddHandlerAfter<Marshaller>(new UserAgentHandler());
+        }
+    }
+
+    /// <summary>
+    /// Adds the Encryption SDK version to the user agent.
+    /// </summary>
+    internal class UserAgentHandler : PipelineHandler
+    {
+        private static readonly string UserAgentSuffix;
+
+        static UserAgentHandler()
+        {
+            AssemblyName assembly = typeof(UserAgentHandler).Assembly.GetName();
+            string version = $"{assembly.Version.Major}.{assembly.Version.Minor}.{assembly.Version.Build}";
+            UserAgentSuffix = $" AwsEncryptionSdkNet/{version}";
+
+            Console.WriteLine(assembly);
+        }
+
+        /// <inheritdoc />
+        public override void InvokeSync(IExecutionContext executionContext)
+        {
+            AddUserAgent(executionContext);
+            base.InvokeSync(executionContext);
+        }
+
+        /// <inheritdoc />
+        public override Task<T> InvokeAsync<T>(IExecutionContext executionContext)
+        {
+            AddUserAgent(executionContext);
+            return base.InvokeAsync<T>(executionContext);
+        }
+
+        protected void AddUserAgent(IExecutionContext executionContext)
+        {
+            var request = executionContext.RequestContext.Request;
+            request.Headers[AWSSDKUtils.UserAgentHeader] += UserAgentSuffix;
         }
     }
 }
