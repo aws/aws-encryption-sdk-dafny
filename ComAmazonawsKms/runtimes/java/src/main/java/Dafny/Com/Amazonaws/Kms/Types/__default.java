68c4ecb55b1bcf798cc9025bfb0314c7dbd5876a
lavaleri <49660121+lavaleri@users.noreply.github.com>
2022-12-02 16:15:15 -0800

    feat: Build KMS Java (#51)

---
+++ ComAmazonawsKms/runtimes/java/src/main/java/Dafny/Com/Amazonaws/Kms/Types/__default.java
@@ -0,0 +1,5 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+package Dafny.Com.Amazonaws.Kms.Types;
+
+public class __default extends Dafny.Com.Amazonaws.Kms.Types._ExternBase___default {}
