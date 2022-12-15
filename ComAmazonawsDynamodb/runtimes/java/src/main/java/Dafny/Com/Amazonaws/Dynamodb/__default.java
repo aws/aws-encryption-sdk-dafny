0bef8024343ac30663a6e8bf7992689df1efec2d
lavaleri <49660121+lavaleri@users.noreply.github.com>
2022-12-14 16:31:27 -0800

    feat: Build DDB smithy->Dafny->Java in CI

---
+++ ComAmazonawsDynamodb/runtimes/java/src/main/java/Dafny/Com/Amazonaws/Dynamodb/__default.java
@@ -0,0 +1,6 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+package Dafny.Com.Amazonaws.Dynamodb;
+
+public class __default extends Dafny.Com.Amazonaws.Dynamodb._ExternBase___default{
+}
