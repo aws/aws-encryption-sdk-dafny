71716c4fcef53af8a62814e7d25918908296f056
lavaleri <49660121+lavaleri@users.noreply.github.com>
2022-12-20 16:25:21 -0800

    feat: Add externs for KMS and UTF8 (#65)
    Co-authored-by: texastony <5892063+texastony@users.noreply.github.com>

--- ComAmazonawsKms/runtimes/java/src/main/java/Dafny/Com/Amazonaws/Kms/__default.java
+++ ComAmazonawsKms/runtimes/java/src/main/java/Dafny/Com/Amazonaws/Kms/__default.java
@@ -2,8 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 package Dafny.Com.Amazonaws.Kms;
 
+import com.amazonaws.regions.DefaultAwsRegionProviderChain;
 import com.amazonaws.services.kms.AWSKMS;
 import com.amazonaws.services.kms.AWSKMSClient;
+import com.amazonaws.services.kms.AWSKMSClientBuilder;
 
 import Dafny.Com.Amazonaws.Kms.Types.Error;
 import Dafny.Com.Amazonaws.Kms.Types.IKeyManagementServiceClient;
@@ -11,20 +13,50 @@ import Wrappers_Compile.Option;
 import Wrappers_Compile.Result;
 import dafny.DafnySequence;
 
+
 import static software.amazon.dafny.conversion.ToDafny.Simple.CharacterSequence;
 import static software.amazon.dafny.conversion.ToNative.Simple.String;
 
 public class __default extends Dafny.Com.Amazonaws.Kms._ExternBase___default{
     public static Result<IKeyManagementServiceClient, Error> KMSClient() {
-        return Result.create_Failure(null);
+        try {
+            // TODO This assumes that the SDK will continue to default to using the DefaulAwsRegionProviderChain.
+            // If this is ever not true in the future, then the behavior here may be surprising.
+            // Determine the likelihood/impact of this changing, and if this is the correct tradeoff to make.
+            String region = new DefaultAwsRegionProviderChain().getRegion();
+
+            AWSKMS client = AWSKMSClientBuilder.standard()
+                                               .withRegion(region)
+                                               .build();
+            IKeyManagementServiceClient shim = new Shim(client, region);
+            return Result.create_Success(shim);
+        } catch (Exception e) {
+            Error dafny_error = Error.create_KMSInternalException(
+                    Option.create_Some(CharacterSequence(e.getMessage())));
+            return Result.create_Failure(dafny_error);
+        }
     }
 
     public static Wrappers_Compile.Option<Boolean> RegionMatch(
             final IKeyManagementServiceClient client,
             final DafnySequence<? extends Character> region
     ) {
+        // We should never be passing anything other than Shim as the 'client'.
+        // If this cast fails, that indicates that there is something wrong with
+        // our code generation.
+        Shim shim = (Shim) client;
+
+        // If the client was created externally we
+        // have no way to determine what region it is
+        // configured with.
+        if (shim.region() == null) {
+            return Option.create_None();
+        }
+
+        // Otherwise we kept record of the region
+        // when we created the client.
+        String shimRegion = shim.region();
         String regionStr = String(region);
-        String actualRegion = ((Shim) client).region();
-        return Option.create_Some(regionStr.equals(actualRegion));
+        return Option.create_Some(regionStr.equals(shimRegion));
     }
 }
--- StandardLibrary/runtimes/java/build.gradle.kts
+++ StandardLibrary/runtimes/java/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.net.URI
+import javax.annotation.Nullable
+
 plugins {
     `java-library`
     `maven-publish`
@@ -13,15 +16,43 @@ java {
         srcDir("src/main/java")
         srcDir("src/main/dafny-generated")
     }
+    sourceSets["test"].java {
+        srcDir("src/test/dafny-generated")
+    }
+}
+
+var caUrl: URI? = null
+@Nullable
+val caUrlStr: String? = System.getenv("CODEARTIFACT_URL_JAVA_CONVERSION")
+if (!caUrlStr.isNullOrBlank()) {
+    caUrl = URI.create(caUrlStr)
+}
+
+var caPassword: String? = null
+@Nullable
+val caPasswordString: String? = System.getenv("CODEARTIFACT_AUTH_TOKEN")
+if (!caPasswordString.isNullOrBlank()) {
+    caPassword = caPasswordString
 }
 
 repositories {
     mavenCentral()
     mavenLocal()
+    if (caUrl != null && caPassword != null) {
+        maven {
+            name = "CodeArtifact"
+            url = caUrl!!
+            credentials {
+                username = "aws"
+                password = caPassword!!
+            }
+        }
+    }
 }
 
 dependencies {
     implementation("dafny.lang:DafnyRuntime:3.10.0")
+    implementation("software.amazon.dafny:conversion:1.0-SNAPSHOT")
 }
 publishing {
     publications.create<MavenPublication>("maven") {
@@ -34,4 +65,11 @@ publishing {
 
 tasks.withType<JavaCompile>() {
     options.encoding = "UTF-8"
+}
+
+tasks {
+    register("runTests", JavaExec::class.java) {
+        mainClass.set("StandardLibraryTests")
+        classpath = sourceSets["test"].runtimeClasspath
+    }
 }
