eeeb0a869b0f2b7449b249d986b14a37ffd930ee
Tony Knapp <5892063+texastony@users.noreply.github.com>
2023-04-07 10:04:47 -0700

    test(MPL-Java): Run MPL Test Vectors in Java (#159)

---
+++ TestVectorsAwsCryptographicMaterialProviders/runtimes/java/build.gradle.kts
@@ -0,0 +1,91 @@
+import java.net.URI
+import javax.annotation.Nullable
+
+tasks.wrapper {
+    gradleVersion = "7.6"
+}
+
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+group = "software.amazon.cryptography"
+version = "1.0-SNAPSHOT"
+description = "TestAwsCryptographicMaterialProviders"
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(8))
+    sourceSets["main"].java {
+        srcDir("src/main/java")
+        srcDir("src/main/dafny-generated")
+        srcDir("src/main/smithy-generated")
+    }
+    sourceSets["test"].java {
+        srcDir("src/test/dafny-generated")
+    }
+}
+
+var caUrl: URI? = null
+@Nullable
+val caUrlStr: String? = System.getenv("CODEARTIFACT_URL_JAVA_CONVERSION")
+if (!caUrlStr.isNullOrBlank()) {
+    caUrl = URI.create(caUrlStr)
+}
+
+var caPassword: String? = null
+@Nullable
+val caPasswordString: String? = System.getenv("CODEARTIFACT_AUTH_TOKEN")
+if (!caPasswordString.isNullOrBlank()) {
+    caPassword = caPasswordString
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    if (caUrl != null && caPassword != null) {
+        maven {
+            name = "CodeArtifact"
+            url = caUrl!!
+            credentials {
+                username = "aws"
+                password = caPassword!!
+            }
+        }
+    }
+}
+
+dependencies {
+    implementation("dafny.lang:DafnyRuntime:3.10.0")
+    implementation("software.amazon.dafny:conversion:1.0-SNAPSHOT")
+    implementation("software.amazon.cryptography:StandardLibrary:1.0-SNAPSHOT")
+    implementation("software.amazon.cryptography:AwsCryptographyPrimitives:1.0-SNAPSHOT")
+    implementation("software.amazon.cryptography:ComAmazonawsKms:1.0-SNAPSHOT")
+    implementation("software.amazon.cryptography:ComAmazonawsDynamodb:1.0-SNAPSHOT")
+    implementation("software.amazon.cryptography:AwsCryptographicMaterialProviders:1.0-SNAPSHOT")
+    implementation(platform("software.amazon.awssdk:bom:2.19.1"))
+    implementation("software.amazon.awssdk:dynamodb")
+    implementation("software.amazon.awssdk:dynamodb-enhanced")
+    implementation("software.amazon.awssdk:kms")
+    implementation("software.amazon.awssdk:core:2.19.1")
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        groupId = group as String?
+        artifactId = description
+        from(components["java"])
+    }
+    repositories { mavenLocal() }
+}
+
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
+
+tasks {
+    register("runTests", JavaExec::class.java) {
+        mainClass.set("TestsFromDafny")
+        classpath = sourceSets["test"].runtimeClasspath
+    }
+}
