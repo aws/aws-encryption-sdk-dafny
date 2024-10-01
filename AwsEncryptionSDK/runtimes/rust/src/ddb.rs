983bff1a5f4a0b148db0f937c0067906a9658e2b
Ritvik Kapila <ritvik.iitd@gmail.com>
2024-10-01 16:51:37 -0700

    fix Makefile; update externs

--- AwsEncryptionSDK/runtimes/rust/src/ddb.rs
+++ AwsEncryptionSDK/runtimes/rust/src/ddb.rs
@@ -6,6 +6,14 @@
 #![deny(clippy::all)]
 
 use aws_config::Region;
+use std::sync::LazyLock;
+
+static DAFNY_TOKIO_RUNTIME: LazyLock<tokio::runtime::Runtime> = LazyLock::new(|| {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+});
 
 #[allow(non_snake_case)]
 impl crate::r#software::amazon::cryptography::services::dynamodb::internaldafny::_default {
@@ -19,11 +27,16 @@ impl crate::r#software::amazon::cryptography::services::dynamodb::internaldafny:
             dafny_runtime::dafny_runtime_conversions::unicode_chars_false::dafny_string_to_string(
                 region,
             );
-        let shared_config = tokio::task::block_in_place(|| {
-            tokio::runtime::Handle::current().block_on(async {
-                aws_config::load_defaults(aws_config::BehaviorVersion::v2024_03_28()).await
-            })
-        });
+        let shared_config = match tokio::runtime::Handle::try_current() {
+            Ok(curr) => tokio::task::block_in_place(|| {
+                curr.block_on(async {
+                    aws_config::load_defaults(aws_config::BehaviorVersion::v2024_03_28()).await
+                })
+            }),
+            Err(_) => DAFNY_TOKIO_RUNTIME.block_on(aws_config::load_defaults(
+                aws_config::BehaviorVersion::v2024_03_28(),
+            )),
+        };
         let shared_config = shared_config
             .to_builder()
             .region(Region::new(region))
@@ -42,11 +55,16 @@ impl crate::r#software::amazon::cryptography::services::dynamodb::internaldafny:
     ::std::rc::Rc<crate::r#software::amazon::cryptography::services::dynamodb::internaldafny::types::Error>
     >
     >{
-        let shared_config = tokio::task::block_in_place(|| {
-            tokio::runtime::Handle::current().block_on(async {
-                aws_config::load_defaults(aws_config::BehaviorVersion::v2024_03_28()).await
-            })
-        });
+        let shared_config = match tokio::runtime::Handle::try_current() {
+            Ok(curr) => tokio::task::block_in_place(|| {
+                curr.block_on(async {
+                    aws_config::load_defaults(aws_config::BehaviorVersion::v2024_03_28()).await
+                })
+            }),
+            Err(_) => DAFNY_TOKIO_RUNTIME.block_on(aws_config::load_defaults(
+                aws_config::BehaviorVersion::v2024_03_28(),
+            )),
+        };
         let inner = aws_sdk_dynamodb::Client::new(&shared_config);
         let client = crate::deps::com_amazonaws_dynamodb::client::Client { inner };
         let dafny_client = ::dafny_runtime::upcast_object()(::dafny_runtime::object::new(client));
--- AwsEncryptionSDK/runtimes/rust/src/kms.rs
+++ AwsEncryptionSDK/runtimes/rust/src/kms.rs
@@ -6,6 +6,14 @@
 #![deny(clippy::all)]
 
 use aws_config::Region;
+use std::sync::LazyLock;
+
+static DAFNY_TOKIO_RUNTIME: LazyLock<tokio::runtime::Runtime> = LazyLock::new(|| {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+});
 
 impl crate::r#software::amazon::cryptography::services::kms::internaldafny::_default {
     #[allow(non_snake_case)]
@@ -14,11 +22,17 @@ impl crate::r#software::amazon::cryptography::services::kms::internaldafny::_def
             dafny_runtime::dafny_runtime_conversions::unicode_chars_false::dafny_string_to_string(
                 region,
             );
-        let shared_config = tokio::task::block_in_place(|| {
-            tokio::runtime::Handle::current().block_on(async {
-                aws_config::load_defaults(aws_config::BehaviorVersion::v2024_03_28()).await
-            })
-        });
+
+        let shared_config = match tokio::runtime::Handle::try_current() {
+            Ok(curr) => tokio::task::block_in_place(|| {
+                curr.block_on(async {
+                    aws_config::load_defaults(aws_config::BehaviorVersion::v2024_03_28()).await
+                })
+            }),
+            Err(_) => DAFNY_TOKIO_RUNTIME.block_on(aws_config::load_defaults(
+                aws_config::BehaviorVersion::v2024_03_28(),
+            )),
+        };
 
         let shared_config = shared_config
             .to_builder()
@@ -34,11 +48,17 @@ impl crate::r#software::amazon::cryptography::services::kms::internaldafny::_def
 
     #[allow(non_snake_case)]
     pub fn KMSClient() -> ::std::rc::Rc<crate::r#_Wrappers_Compile::Result<::dafny_runtime::Object<dyn crate::software::amazon::cryptography::services::kms::internaldafny::types::IKMSClient>, ::std::rc::Rc<crate::software::amazon::cryptography::services::kms::internaldafny::types::Error>>>{
-        let shared_config = tokio::task::block_in_place(|| {
-            tokio::runtime::Handle::current().block_on(async {
-                aws_config::load_defaults(aws_config::BehaviorVersion::v2024_03_28()).await
-            })
-        });
+        let shared_config = match tokio::runtime::Handle::try_current() {
+            Ok(curr) => tokio::task::block_in_place(|| {
+                curr.block_on(async {
+                    aws_config::load_defaults(aws_config::BehaviorVersion::v2024_03_28()).await
+                })
+            }),
+            Err(_) => DAFNY_TOKIO_RUNTIME.block_on(aws_config::load_defaults(
+                aws_config::BehaviorVersion::v2024_03_28(),
+            )),
+        };
+
         let inner = aws_sdk_kms::Client::new(&shared_config);
         let client = crate::deps::com_amazonaws_kms::client::Client { inner };
         let dafny_client = ::dafny_runtime::upcast_object()(::dafny_runtime::object::new(client));
