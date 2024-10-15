bfb7c0cf917d50f452584f9fe0af115d93663ce3
Ritvik Kapila <ritvik.iitd@gmail.com>
2024-10-15 01:07:18 -0700

    update examples/main.rs

--- AwsEncryptionSDK/runtimes/rust/examples/main.rs
+++ AwsEncryptionSDK/runtimes/rust/examples/main.rs
@@ -9,7 +9,9 @@ pub mod client_supplier;
 pub mod cryptographic_materials_manager;
 pub mod keyring;
 pub mod example_utils;
-
+pub mod set_encryption_algorithm_suite_example;
+pub mod set_commitment_policy_example;
+pub mod limit_encrypted_data_keys_example;
 use std::convert::From;
 
 // Why two types?
@@ -42,5 +44,145 @@ impl<T: std::fmt::Debug> From<T> for BoxError {
 
 #[tokio::main]
 pub async fn main() -> Result<(), BoxError2> {
+    use example_utils::utils;
+
+    keyring::aws_kms_discovery_keyring_example::encrypt_and_decrypt_with_keyring(
+        utils::TEST_EXAMPLE_DATA,
+        utils::TEST_DEFAULT_KMS_KEY_ID,
+        utils::TEST_DEFAULT_KMS_KEY_ACCOUNT_ID
+    ).await?;
+
+    let aws_regions: Vec<String> = vec!["us-east-1".to_string(), "us-west-2".to_string()];
+
+    keyring::aws_kms_discovery_multi_keyring_example::encrypt_and_decrypt_with_keyring(
+        utils::TEST_EXAMPLE_DATA,
+        utils::TEST_DEFAULT_KMS_KEY_ID,
+        utils::TEST_DEFAULT_KMS_KEY_ACCOUNT_ID,
+        aws_regions
+    ).await?;
+
+    keyring::aws_kms_keyring_example::encrypt_and_decrypt_with_keyring(
+        utils::TEST_EXAMPLE_DATA,
+        utils::TEST_DEFAULT_KMS_KEY_ID
+    ).await?;
+
+    let mrk_encrypt_region: String = "us-east-1".to_string();
+    let mrk_replica_decrypt_region: String = "eu-west-1".to_string();
+
+    keyring::aws_kms_mrk_discovery_keyring_example::encrypt_and_decrypt_with_keyring(
+        utils::TEST_EXAMPLE_DATA,
+        utils::TEST_MRK_KEY_ID_US_EAST_1,
+        utils::TEST_DEFAULT_KMS_KEY_ACCOUNT_ID,
+        mrk_encrypt_region,
+        mrk_replica_decrypt_region,
+    ).await?;
+
+    let mrk_encrypt_region: String = "us-east-1".to_string();
+    let aws_regions: Vec<String> = vec!["us-east-1".to_string(), "us-west-2".to_string()];
+
+    keyring::aws_kms_mrk_discovery_multi_keyring_example::encrypt_and_decrypt_with_keyring(
+        utils::TEST_EXAMPLE_DATA,
+        utils::TEST_MRK_KEY_ID_US_EAST_1,
+        mrk_encrypt_region,
+        utils::TEST_DEFAULT_KMS_KEY_ACCOUNT_ID,
+        aws_regions,
+    ).await?;
+
+    let mrk_encrypt_region: String = "us-east-1".to_string();
+    let mrk_replica_decrypt_region: String = "eu-west-1".to_string();
+
+    keyring::aws_kms_mrk_keyring_example::encrypt_and_decrypt_with_keyring(
+        utils::TEST_EXAMPLE_DATA,
+        utils::TEST_MRK_KEY_ID_US_EAST_1,
+        utils::TEST_MRK_KEY_ID_EU_WEST_1,
+        mrk_encrypt_region,
+        mrk_replica_decrypt_region,
+    ).await?;
+
+    let mrk_replica_decrypt_region: String = "eu-west-1".to_string();
+
+    keyring::aws_kms_mrk_multi_keyring_example::encrypt_and_decrypt_with_keyring(
+        utils::TEST_EXAMPLE_DATA,
+        utils::TEST_MRK_KEY_ID_US_EAST_1,
+        utils::TEST_DEFAULT_KMS_KEY_ID,
+        utils::TEST_MRK_KEY_ID_EU_WEST_1,
+        mrk_replica_decrypt_region,
+    ).await?;
+
+    let default_region: String = "us-west-2".to_string();
+    let second_region: String = "eu-central-1".to_string();
+
+    keyring::aws_kms_multi_keyring_example::encrypt_and_decrypt_with_keyring(
+        utils::TEST_EXAMPLE_DATA,
+        utils::TEST_DEFAULT_KMS_KEY_ID,
+        utils::TEST_SECOND_REGION_KMS_KEY_ID,
+        default_region,
+        second_region,
+    ).await?;
+
+    keyring::aws_kms_rsa_keyring_example::encrypt_and_decrypt_with_keyring(
+        utils::TEST_EXAMPLE_DATA,
+        utils::TEST_KMS_RSA_KEY_ID,
+        utils::TEST_KMS_RSA_PUBLIC_KEY
+    ).await?;
+
+    keyring::multi_keyring_example::encrypt_and_decrypt_with_keyring(
+        utils::TEST_EXAMPLE_DATA,
+        utils::TEST_DEFAULT_KMS_KEY_ID
+    ).await?;
+
+    keyring::raw_aes_keyring_example::encrypt_and_decrypt_with_keyring(
+        utils::TEST_EXAMPLE_DATA,
+    ).await?;
+
+    keyring::raw_rsa_keyring_example::encrypt_and_decrypt_with_keyring(
+        utils::TEST_EXAMPLE_DATA,
+    ).await?;
+
+    keyring::aws_kms_hierarchical::aws_kms_hierarchical_keyring_example::encrypt_and_decrypt_with_keyring(
+        utils::TEST_EXAMPLE_DATA,
+        utils::TEST_KEY_STORE_NAME,
+        utils::TEST_LOGICAL_KEY_STORE_NAME,
+        utils::TEST_KEY_STORE_KMS_KEY_ID
+    ).await?;
+
+    cryptographic_materials_manager::required_encryption_context::required_encryption_context_example::encrypt_and_decrypt_with_cmm(
+        utils::TEST_EXAMPLE_DATA,
+        utils::TEST_DEFAULT_KMS_KEY_ID
+    ).await?;
+
+    cryptographic_materials_manager::restrict_algorithm_suite::signing_only_example::encrypt_and_decrypt_with_cmm(
+        utils::TEST_EXAMPLE_DATA,
+        utils::TEST_DEFAULT_KMS_KEY_ID
+    ).await?;
+
+    let aws_regions: Vec<String> = vec!["eu-west-1".to_string()];
+
+    client_supplier::client_supplier_example::encrypt_and_decrypt_with_keyring(
+        utils::TEST_EXAMPLE_DATA,
+        utils::TEST_MRK_KEY_ID_US_EAST_1,
+        utils::TEST_DEFAULT_KMS_KEY_ACCOUNT_ID,
+        aws_regions,
+    ).await?;
+
+    set_encryption_algorithm_suite_example::encrypt_and_decrypt_with_keyring(
+        utils::TEST_EXAMPLE_DATA,
+    ).await?;
+
+    set_commitment_policy_example::encrypt_and_decrypt_with_keyring(
+        utils::TEST_EXAMPLE_DATA,
+        utils::TEST_DEFAULT_KMS_KEY_ID
+    ).await?;
+
+    // max_encrypted_data_keys MUST be greater than 0
+    let max_encrypted_data_keys: u16 = 3;
+
+    limit_encrypted_data_keys_example::encrypt_and_decrypt_with_keyring(
+        utils::TEST_EXAMPLE_DATA,
+        max_encrypted_data_keys
+    ).await?;
+
+    println!("All examples completed successfully.");
+
     Ok(())
 }
